4cd7944448fab11ab75ddaae156d089dc0e851fc
Mark Ellis <markellisdev@gmail.com>
2020-06-04 15:58:10 -0500

    Solved Exercise 1.5, bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+height = float(100)
+bounce = float(3/5)
+num_of_rebounds = 1
+num_of_falls = 10
+
+while num_of_rebounds <= num_of_falls:
+    print(round((height * bounce), 4))
+    height = height * bounce
+    num_of_rebounds += 1
